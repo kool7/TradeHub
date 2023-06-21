46186d4a0c27f3f7ef49e30946bcd3fcc19f0a23
Kuldeep Singh Chouhan <kschouhan714@gmail.com>
2023-06-21 22:07:21 +0530

    added unit tests for dbcontext

--- tests/Unit/UserHub.UnitTests/Repositories/UserRepositoryTests.cs
+++ tests/Unit/UserHub.UnitTests/Repositories/UserRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoFixture;
 using FluentAssertions;
+using MentorsManagement.UnitTests.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using UserHub.Api.Data;
@@ -12,28 +13,36 @@ namespace UserHub.UnitTests.Repositories
     {
         private readonly Fixture _fixture;
         private readonly Mock<UserHubDbContext> _userHubDbContext;
-        private readonly UserRepository _userRepository;
+        private readonly UserRepositoryDb _userRepository;
 
         public UserRepositoryTests()
         {
             var dbContextOptions = new DbContextOptionsBuilder<UserHubDbContext>()
-                .UseSqlServer("UserHubTestDb")
+                .UseInMemoryDatabase("UserHubTesting")
                 .Options;
 
             _fixture = new Fixture();
             _userHubDbContext = new Mock<UserHubDbContext>(dbContextOptions);
-            _userRepository = new UserRepository(_userHubDbContext.Object);
+            _userRepository = new UserRepositoryDb(_userHubDbContext.Object);
         }
 
         [Fact]
         public async Task GetAll_Users_ReturnListOfUsers()
         {
             // Arrange
-            var users = _fixture.CreateMany<User>(3);
-            foreach (var user in users)
-            {
-                await _userRepository.CreateUser(user);
-            }
+            var users = _fixture.CreateMany<User>(3).ToList();
+            var mockDbSet = new Mock<DbSet<User>>();
+            var queryableData = users.AsQueryable();
+
+            mockDbSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(queryableData.Provider);
+            mockDbSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(queryableData.Expression);
+            mockDbSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(queryableData.ElementType);
+            mockDbSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(queryableData.GetEnumerator());
+            mockDbSet.As<IAsyncEnumerable<User>>()
+                .Setup(m => m.GetAsyncEnumerator(It.IsAny<CancellationToken>()))
+                .Returns(new MockAsyncEnumerator<User>(queryableData.GetEnumerator()));
+
+            _userHubDbContext.Setup(c => c.Users).Returns(mockDbSet.Object);
 
             // Act
             var result = await _userRepository.GetAll();
@@ -47,27 +56,15 @@ namespace UserHub.UnitTests.Repositories
         {
             // Arrange
             var user = _fixture.Create<User>();
-            await _userRepository.CreateUser(user);
+            var mockDbSet = new Mock<DbSet<User>>();
+            _userHubDbContext.Setup(s => s.Users).Returns(mockDbSet.Object);
 
             // Act
             var result = await _userRepository.GetUser(user.Id);
 
             // Assert
             result.Should().Be(user);
-            result.Should().BeOfType<User>();
-        }
-
-        [Fact]
-        public async Task GetUser_IfUserDoesNotExists_ReturnNull()
-        {
-            // Arrange
-            var userId = Guid.NewGuid();
-
-            // Act
-            var result = await _userRepository.GetUser(userId);
-
-            // Assert
-            result.Should().BeNull();
+            mockDbSet.Verify(m => m.Add(It.IsAny<User>()), Times.Once());
         }
 
         [Fact]
@@ -84,11 +81,11 @@ namespace UserHub.UnitTests.Repositories
         }
 
         [Fact]
-        public void DeleteUser_RemovesUserFromList_ReturnTrue()
+        public async Task DeleteUser_RemovesUserFromList_ReturnTrue()
         {
             // Arrange
             var user = _fixture.Create<User>();
-            _userRepository.CreateUser(user);
+            await _userRepository.CreateUser(user);
 
             // Act
             var result = _userRepository.DeleteUser(user);
